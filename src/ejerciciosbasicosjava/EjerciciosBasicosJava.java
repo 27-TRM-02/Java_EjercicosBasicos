2cc9e53a0730b37de477ed18e47bb2aa7e3ad125
TRM <trms2702@gmail.com>
2021-01-20 12:34:39 +0100

    Primeros 5 ejercicios

--- src/ejerciciosbasicosjava/EjerciciosBasicosJava.java
+++ src/ejerciciosbasicosjava/EjerciciosBasicosJava.java
@@ -28,7 +28,12 @@ public class EjerciciosBasicosJava {
         return false;
     }
     
-    // Si la velocidad es 
+    /**
+     * 
+     * @param velocidad
+     * @param birthday
+     * @return 
+     */
     public int multa(int velocidad, boolean birthday){
         // si es tu cumple, le resta 5 a la velocidad
         if (birthday){
@@ -46,6 +51,46 @@ public class EjerciciosBasicosJava {
         return 2;
     }
     
+    /**
+     * 
+     * @param n
+     * @return 
+     */
+    public boolean muyVanidoso(int n){
+        if ((n % 11) == 0 || ((n % 11) == 1)){
+            return true;
+        } 
+        return false;
+    }
+    
+    /**
+     * 
+     * @param matinal
+     * @param madre
+     * @param dormido
+     * @return 
+     */
+    public boolean contesta(boolean matinal, boolean madre, boolean dormido){
+        if (dormido){
+            return false;
+        }else if (matinal && !madre){
+            return false;
+        }
+        return true;
+    }
+    
+    public boolean menorPor10(int a, int b, int c){
+        if (a + 10 == b || b + 10 == a){
+            return true;
+        }
+        if (b + 10 == c || c + 10 == b){
+            return true;
+        }
+        if (c + 10 == a || a + 10 == c){
+            return true;
+        }
+        return false;
+    }
 
     /**
      * @param args the command line arguments
@@ -54,17 +99,33 @@ public class EjerciciosBasicosJava {
         // creamos objeto de esta clase (Ejercicios basicosJava)
         EjerciciosBasicosJava ejercicio = new EjerciciosBasicosJava();
         // Ejercicio fiesta de ardillas
+        System.out.println("EJERCICIO 01 - FIESTA DE ARDILLAS");
         System.out.println(ejercicio.fiestaArdillas(30, false));
         System.out.println(ejercicio.fiestaArdillas(50, false));
         System.out.println(ejercicio.fiestaArdillas(70, true));
         
         // Ejercicio de multa
-        System.out.println(ejercicio.multa(60, true));
+        System.out.println("EJERCICIO 02 - MULTA");
+        System.out.println(ejercicio.multa(60, false));
         System.out.println(ejercicio.multa(65, false));
-        System.out.println(ejercicio.multa(65, false));
-        System.out.println(ejercicio.multa(85, false));
-
-
+        System.out.println(ejercicio.multa(65, true));   
+        
+        // Ejercicio muyVanidoso, numero multiplo + 1 de 11
+        System.out.println("EJERCICIO 03 - MUY VANIDOSO");
+        System.out.println(ejercicio.muyVanidoso(22));
+        System.out.println(ejercicio.muyVanidoso(23));
+        System.out.println(ejercicio.muyVanidoso(24));
+        
+        System.out.println("EJERCICIO 04 - CONTESTA AL MOVIL");
+        System.out.println(ejercicio.contesta(false, false, false));
+        System.out.println(ejercicio.contesta(false, false, true));
+        System.out.println(ejercicio.contesta(true, false, false));
+        
+        System.out.println("EJERCICIO 05 - DIFERENCIA DE 10");
+        System.out.println(ejercicio.menorPor10(1, 7, 11));
+        System.out.println(ejercicio.menorPor10(1, 7, 10));
+        System.out.println(ejercicio.menorPor10(11, 1, 7));
+        
     }
     
 }
